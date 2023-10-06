c176c6a81599dd2334818e10c62a5e024ed19334
John Fessler <jffessler@gmail.com>
2023-10-05 21:25:26 -0700

    continuing with course, beginning to look into handling and training a dataset

--- MAGIC.py
+++ MAGIC.py
@@ -7,7 +7,20 @@ cols = ["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans"
 df = pd.read_csv("magic04.data", names=cols)
 
 df["class"] = (df["class"] == "g").astype(int)
-print(df)
-# print(df.head())
+# print(df)
+# train_df = df.head()
 # print(df["class"].unique())
+# print(train_df)
 
+# for label in cols[:-1]:
+#     plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha = 0.7, density=True)
+#     plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha = 0.7, density=True)
+#     plt.title(label)
+#     plt.ylabel("Probability")
+#     plt.xlabel(label)
+#     plt.legend()
+#     plt.show()
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+print(len(train),len(valid),len(test))
