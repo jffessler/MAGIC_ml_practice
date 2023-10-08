dc9596bebabdd367f7b0b2e304b195b1cdf6750d
John Fessler <jffessler@gmail.com>
2023-10-07 17:19:11 -0700

    built framework for testing

--- MAGIC.py
+++ MAGIC.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
 
 
 cols = ["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
@@ -23,4 +25,28 @@ df["class"] = (df["class"] == "g").astype(int)
 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
-print(len(train),len(valid),len(test))
+# print(len(train),len(valid),len(test))
+
+def scale_dataset(dataframe, oversample = False):
+    X = dataframe[dataframe.columns[:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    if oversample:
+        ros = RandomOverSampler()
+        X, y = ros.fit_resample(X,y)
+
+    data = np.hstack((X,np.reshape(y,(-1,1))))
+    return data,X,y
+
+train,X_train,y_train = scale_dataset(train, oversample=True)
+valid,X_valid,y_valid = scale_dataset(valid, oversample=False)
+test,X_test,y_test = scale_dataset(test, oversample=False)
+
+
+# print(len(y_train))
+
+# print(sum(y_train==1))
+# print(sum(y_train==0))
