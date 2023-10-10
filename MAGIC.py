013dd062b841a89760619fa25c5268d670bc8092
John Fessler <jffessler@gmail.com>
2023-10-09 21:52:37 -0700

    finished section on tensorflow

--- MAGIC.py
+++ MAGIC.py
@@ -48,7 +48,7 @@ test,X_test,y_test = scale_dataset(test, oversample=False)
 ###### kNN modeling
 
 # from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report
 
 # knn_model = KNeighborsClassifier(n_neighbors=5)
 # knn_model.fit(X_train,y_train)
@@ -90,32 +90,61 @@ test,X_test,y_test = scale_dataset(test, oversample=False)
 
 import tensorflow as tf
 
-def plot_loss(history):
-    plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['val_loss'], label='val_loss')
-    plt.xlabel("Epoch")
-    plt.ylabel("Binary Crossentropy")
-    plt.legend()
-    plt.grid(True)
+def plot_history(history):
+    fig, (ax1,ax2) = plt.subplots(1, 2, figsize =(10,4))
+    ax1.plot(history.history['loss'], label='loss')
+    ax1.plot(history.history['val_loss'], label='val_loss')
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Binary Crossentropy")
+    ax1.grid(True)
+
+    ax2.plot(history.history['accuracy'], label='accuracy')
+    ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Accuracy")
+    ax2.grid(True)
+
     plt.show()
 
-def plot_accuracy(history):
-    plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label='val_accuracy')
-    plt.xlabel("Epoch")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# def plot_accuracy(history):
+#     plt.plot(history.history['accuracy'], label='accuracy')
+#     plt.plot(history.history['val_accuracy'], label='val_accuracy')
+#     plt.xlabel("Epoch")
+#     plt.ylabel("Accuracy")
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
 
-nn_model = tf.keras.Sequential([
-    tf.keras.layers.Dense(32, activation="relu", input_shape = (10,)),
-    tf.keras.layers.Dense(32, activation="relu"),
-    tf.keras.layers.Dense(1, activation="sigmoid")
-])
-nn_model.compile(optimizer=tf.keras.optimizers.legacy.Adam(0.001), loss="binary_crossentropy", metrics=["accuracy"])
+def train_model(X_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs):
+    nn_model = tf.keras.Sequential([
+        tf.keras.layers.Dense(num_nodes, activation="relu", input_shape = (10,)),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(num_nodes, activation="relu"),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+    nn_model.compile(optimizer=tf.keras.optimizers.legacy.Adam(lr), loss="binary_crossentropy", metrics=["accuracy"])
 
-history = nn_model.fit(X_train,y_train,epochs=100,batch_size=32,validation_split=0.2,verbose=0)
+    history = nn_model.fit(X_train,y_train,epochs=epochs,batch_size=batch_size,validation_split=0.2,verbose=0)
+    return nn_model, history
 
-plot_loss(history)
-plot_accuracy(history)
+least_val_loss = float('inf')
+least_loss_model = None
+
+epochs = 100
+for num_nodes in [16, 32, 64]:
+    for dropout_prob in [0, 0.2]:
+        for lr in [0.01, 0.005, 0.001]:
+            for batch_size in [32, 64, 128]:
+                print(f"{num_nodes} number of nodes, {dropout_prob} dropout, {lr} lr, {batch_size} batch size")
+                model, history = train_model(X_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs)
+                plot_history(history)
+                val_loss = model.evaluate(X_valid, y_valid)[0]
+                if val_loss < least_val_loss:
+                    least_val_loss = val_loss
+                    least_loss_model = model
+
+y_pred = least_loss_model.predict(X_test)
+y_pred = (y_pred > 0.5).astype(int).reshape(-1,)
+
+print(classification_report(y_test, y_pred))
