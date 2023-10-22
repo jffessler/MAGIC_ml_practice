7fbab15ad1ba50ea7838ed910094a0828e8598cb
John Fessler <jffessler@gmail.com>
2023-10-22 14:00:36 -0700

    continuing with course, linear regression of bike model

--- fcc_bikes_regression.py
+++ fcc_bikes_regression.py
@@ -16,7 +16,7 @@ df["functional"] = (df["functional"]=="Yes").astype(int)
 df = df[df["hour"]==12]
 df = df.drop(["hour"], axis=1)
 df = df.drop(["wind","visibility", "functional"], axis=1)
-print(df.head())
+# print(df.head())
 
 # for label in df.columns[1:]:
 #     plt.scatter(df[label], df["bike_count"])
@@ -24,3 +24,42 @@ print(df.head())
 #     plt.ylabel("Bike count at noon")
 #     plt.xlabel(label)
 #     plt.show()
+
+#### Train/validate/test
+
+train, val, test = np.split(df.sample(frac = 1), [int(0.6*len(df)), int(0.8*len(df))])
+
+def get_xy(dataframe, y_label, x_labels = None):
+    dataframe = copy.deepcopy(dataframe)
+    if not x_labels:
+        X = dataframe[[c for c in dataframe.columns if c != y_label]].values
+    else:
+        if len(x_labels) == 1:
+            X = dataframe[x_labels[0]].values.reshape(-1,1)
+        else:
+            X = dataframe[x_labels].values
+    y = dataframe[y_label].values.reshape(-1,1)
+    data = np.hstack((X, y))
+    return data, X, y
+
+_, X_train_temp, y_train_temp = get_xy(train, "bike_count", x_labels=["temp"])
+_, X_val_temp, y_val_temp = get_xy(val, "bike_count", x_labels=["temp"])
+_, X_test_temp, y_test_temp = get_xy(test, "bike_count", x_labels=["temp"])
+
+temp_reg =  LinearRegression()
+temp_reg.fit(X_train_temp, y_train_temp)
+
+# print(temp_reg.coef_, temp_reg.intercept_)
+# print(temp_reg.score(X_test_temp,y_test_temp))
+
+# plt.scatter(X_train_temp,y_train_temp, label = "Data", color = "blue")
+# x = tf.linspace(-20, 40, 100)
+# plt.plot(x, temp_reg.predict(np.array(x).reshape(-1,1)), label = "Fit", color = "red", linewidth = 3)
+# plt.legend()
+# plt.title("Bikes vs Temp")
+# plt.ylabel("Number of Bikes")
+# plt.xlabel("Temp")
+# plt.show()
+
+#### Multiple Linear Regression 
+
